14dd7ce7abc54ee4dfe419f8eb674d458f5b2d16
ramicoder <ramidaood@anonymous>
2026-05-07 13:50:19 +0300

    implemented the general layout using grid properties in css as well as proper classes and elements in html

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 const myLibrary = [];
 
-function Book(title, author, pages, publishDate) {
+function Book(title, author, pages, publishDate, hasBeenRead) {
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.publishDate = publishDate;
+    this.hasBeenRead = hasBeenRead;
 }
 
-function addBook(title, author, pages, publishDate) {
-    const newBook = new Book(title, author, pages, publishDate);
+function addBook(title, author, pages, publishDate, hasBeenRead) {
+    const newBook = new Book(title, author, pages, publishDate, hasBeenRead);
     myLibrary.push(newBook);
 }
 
 function displayBooks(books) {
     for (let book of books) {
-        console.log(`Title: ${book.title}\nAuthor: ${book.author}\nPages: ${book.pages}\nDate of publication: ${book.publishDate}\n\n`);
+        console.log(`Title: ${book.title}\nAuthor: ${book.author}\nPages: ${book.pages}\nDate of publication: ${book.publishDate}\nHas been read: ${book.hasBeenRead}\n\n`);
     }
 }
 
-addBook("40rules", "Rami", 67, 2006);
-addBook("richdad", "kako", 96, 2026);
+addBook("40rules", "Rami", 67, 2006, "yes");
+addBook("richdad", "kako", 96, 2026, "no");
 displayBooks(myLibrary);
