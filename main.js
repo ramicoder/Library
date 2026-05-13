4ce82cb23f8179c4cb7355f257b6b4a71b6d0d50
ramicoder <ramidaood@anonymous>
2026-05-13 12:46:59 +0300

    some debugging in order to make the logic flow between both pages

--- main.js
+++ main.js
@@ -9,18 +9,21 @@ function Book(title, author, pages, publishDate, hasBeenRead) {
     this.hasBeenRead = hasBeenRead;
 }
 
+
 const form = document.getElementById("book-form");
 
 if (form) {
     form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-    const page = document.getElementById("pages").value;
-    const publication = document.getElementById("publication").value;
-    const readStatus = document.querySelector('input[name="read"]:checked').value;
-    addBook(title, author, page, publication,readStatus);
-});
+        e.preventDefault();
+        const title = document.getElementById("title").value;
+        const author = document.getElementById("author").value;
+        const page = document.getElementById("pages").value;
+        const publication = document.getElementById("publication").value;
+        const readStatus = document.querySelector('input[name="read"]:checked').value;
+        addBook(title, author, page, publication, readStatus);
+        form.reset();
+        console.log(myLibrary);
+    });
 }
 
 
@@ -30,11 +33,11 @@ function addBook(title, author, pages, publishDate, hasBeenRead) {
 }
 
 display = document.querySelector(".card.system-log");
-
 addBook("40rules", "Rami", 69, 1996, true);
 
 function displayBooks(books) {
     for (let book of books) {
+         console.log(myLibrary);
 
         const bookCard = document.createElement("div");
         bookCard.className = "book-card";
@@ -42,16 +45,16 @@ function displayBooks(books) {
         text.className = "descript";
 
         text.textContent =
-        `Title: ${book.title}
-        Author: ${book.author}
-        Pages: ${book.pages}
-        Published in: ${book.publishDate}
-        Has been read: ${book.hasBeenRead}`;
+            `Title: ${book.title}
+            Author: ${book.author}
+            Pages: ${book.pages}
+            Published in: ${book.publishDate}
+            Has been read: ${book.hasBeenRead}`;
 
         const removeBtn = document.createElement("button");
         removeBtn.className = "newBtn";
         removeBtn.textContent = "Remove";
-        
+
 
         const readBtn = document.createElement("button");
         readBtn.className = "newBtn";
@@ -61,12 +64,22 @@ function displayBooks(books) {
         bookCard.appendChild(text);
         bookCard.appendChild(removeBtn);
         bookCard.appendChild(readBtn);
+        if (display) {
+            display.appendChild(bookCard);
+        }
 
-        display.appendChild(bookCard);
-        
     }
 }
 
+const displayBtn = document.querySelector(".displayBtn");
+if (displayBtn) {
+    displayBtn.addEventListener("click", () => {
+    display.innerHTML = "";
     displayBooks(myLibrary);
+});
+}
+
+ 
+
 
 
